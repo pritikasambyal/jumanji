eeecddc2b1b225406f09130c6e472ee8246f7a75
Pritika.sambyal <Pritika.sambyal@rci.rogers.com>
2019-03-26 10:58:11 -0400

    Initial commit

---
+++ src/main/java/com/game/jumanji/JumanjiApplication.java
@@ -0,0 +1,13 @@
+package com.game.jumanji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumanjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumanjiApplication.class, args);
+	}
+
+}
